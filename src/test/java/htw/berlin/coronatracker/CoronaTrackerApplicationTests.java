1a0d1a51a88f59d4cc7fd3204b124f936e51b9c5
Daniel Edlich <48770657+DanielEdlich@users.noreply.github.com>
2021-07-21 10:02:30 +0200

    Initial commit with Spring

---
+++ src/test/java/htw/berlin/coronatracker/CoronaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.coronatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
